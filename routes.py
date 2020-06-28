fc18aaa8cab193dffdbc6524585786072489a86b
AndrewJonesPSU <58799343+AndrewJonesPSU@users.noreply.github.com>
2020-06-28 01:37:34 -0400

    added shutdown server command

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from main import app
+import os
 
 # route to mine/POST a blok
 	
@@ -9,3 +10,12 @@ from main import app
 # route to resolve neighboring chains (get the longest one from friends)
 
 # route to shut down the server (for development purposes, don't use in actual apps unless you want anyone at all to be able to turn off your server at any time)
+@app.route("/terminate", methods=["GET"])
+def shutdownServer():
+	try:
+		CTRL_C_EVENT = 0
+		GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0)
+		return "Worked"
+	except Exception as e:
+		os._exit(0)
+		return e
